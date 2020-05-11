faa6b43fe72a1e7ace15a39704707977565d9932
Sanjeev Kumar <sanjeevkumar761@gmail.com>
2020-05-11 22:21:17 +0200

    Update landingzone.tf
    Added vnet and subnet creation coding block for SAP on Azure.

--- landingzones/landingzone_starter/landingzone.tf
+++ landingzones/landingzone_starter/landingzone.tf
@@ -1,1 +1,27 @@
 ## declare here the resources for your landing zone
+
+resource "azurerm_resource_group" "sap_rg" {
+  name     = "sap_rg"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "sap_vnet" {
+  name                = "sap_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.sap_rg.location
+  resource_group_name = azurerm_resource_group.sap_rg.name
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "app_subnet"
+  resource_group_name  = azurerm_resource_group.sap_rg.name
+  virtual_network_name = azurerm_virtual_network.sap_vnet.name
+  address_prefix     = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "db_subnet" {
+  name                 = "db_subnet"
+  resource_group_name  = azurerm_resource_group.sap_rg.name
+  virtual_network_name = azurerm_virtual_network.sap_vnet.name
+  address_prefix     = "10.0.2.0/24"
+}
